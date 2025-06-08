699668aa1f845d83343974b91a3ddfdce2ed0e48
airbnb55260 <airbnb55260@gmail.com>
2025-06-09 00:03:58 +0500

    Updated FreelancerProfile.tsx

--- src/pages/FreelancerProfile.tsx
+++ src/pages/FreelancerProfile.tsx
@@ -143,8 +143,8 @@ I'm passionate about creating digital experiences that not only look great but a
                 />
                 {freelancer.verified && (
                   <div className="absolute -bottom-2 -right-2 w-10 h-10 bg-green-500 rounded-full flex items-center justify-center border-4 border-white">
-                    <svg className="w-5 h-5 text-white" fill="currentColor" viewBox="0 0 20 20">
-                      <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
+                    <svg className="w-5 h-5 text-white\" fill="currentColor\" viewBox="0 0 20 20">
+                      <path fillRule="evenodd\" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z\" clipRule="evenodd" />
                     </svg>
                   </div>
                 )}
